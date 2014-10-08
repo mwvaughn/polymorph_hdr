8f7aa858856b645a7d6b54a3b2c0682443e65791
Matthew Vaughn <vaughn@tacc.utexas.edu>
2014-10-08 04:10:12 -0500

    Mapping out future work and going back to bed

--- main.py
+++ main.py
@@ -10,20 +10,30 @@ http://polymorph.weigelworld.org/cgi-bin/webapp.cgi?page=generic_region_search&p
 def search(arg):
     
     # Extract query parameters
+    # Detect whether client is searching by locus or position
     search_mode = 0
     '''
     if arg['locus']:
+        Todo: Validate locus via regex
         locus = arg['locus']
         search_mode = 1 # by locus
     elif arg['chromosome']:
+        # Todo: Validate chromosome
         chrom = arg['chromosome']
         start_position = arg['start']
         stop_postiion = arg['stop']    
         search_mode = 2 # by position
-    '''    
+    '''
+    # Todo: Extract and validate accessions against known list 
+    # Todo: Accept and validate project ID
+    
     payload = {'page': 'generic_region_search', 'plugin': 'retrieve_hdr', 'project': 'MPICao2010', 'rm': 'compute', 'accessions_list1': ['Agu-1', 'Bak-2', 'Bak-7', 'Cdm-0', 'Del-10', 'Dog-4', 'Don-0', 'Ey15-2', 'Fei-0', 'HKT2.4', 'ICE1', 'ICE102', 'ICE104', 'ICE106'], 'download': 'csv', 'tair_id': 'At4g25530'}
     r = requests.get(polymorph_common.base_url(), params=payload)
     print(r.url)
+    
+    # Todo: Fetch results, return as AIP feature objects, which are convertible to GFF
+    # Todo: Speak with @waltermoreira about composition of services - I think we have
+    #       our first use case. Conversion of AIP feature objects to GFF on the fly
     
     pass
 
